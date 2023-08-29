a1db6aba3e5191a3a6b870b5505d08ebcd1995ef
Nudlik <gegorp@mail.com>
2023-08-29 18:16:44 +0700

    implemented class queue.py

--- src/queue.py
+++ src/queue.py
@@ -7,7 +7,8 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Queue:
@@ -15,7 +16,8 @@ class Queue:
 
     def __init__(self):
         """Конструктор класса Queue"""
-        pass
+        self.head = None
+        self.tail = None
 
     def enqueue(self, data):
         """
@@ -23,7 +25,12 @@ class Queue:
 
         :param data: данные, которые будут добавлены в очередь
         """
-        pass
+        if self.head is None:
+            self.head = Node(data, None)
+            self.tail = self.head
+        else:
+            self.tail.next_node = Node(data, None)
+            self.tail = self.tail.next_node
 
     def dequeue(self):
         """
@@ -31,8 +38,23 @@ class Queue:
 
         :return: данные удаленного элемента
         """
-        pass
+        if self.head is None:
+            return None
+
+        data = self.head.data
+        self.head = self.head.next_node
+
+        return data
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
-        pass
+        node = self.head
+        if node is None:
+            return ''
+
+        res = []
+        while node:
+            res.append(node.data)
+            node = node.next_node
+
+        return '\n'.join(res)
